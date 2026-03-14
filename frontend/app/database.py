d67bcca3b90f46da510fdf43579715f104c6ef87
Rohith1507-code <rohithmurali2005@gmail.com>
2026-03-14 12:41:23 +0530

    Delete frontend/app directory

--- frontend/app/database.py
+++
@@ -1,9 +0,0 @@
-from pymongo import MongoClient
-
-MONGO_URL = "mongodb+srv://grievanceAdmin:yash%407892@cluster0.nbohumo.mongodb.net/grievance_db?retryWrites=true&w=majority&appName=Cluster0"
-
-client = MongoClient(MONGO_URL)
-
-db = client["grievance_db"]
-
-complaints_collection = db["complaints"]
